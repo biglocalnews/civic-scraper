4fede6aba42e06af84287e481085983b80f3a8da
Amy DiPierro <adipier1@gmail.com>
2020-06-26 06:08:15 -0700

    Streamlining CivicPlusSite
    Moves some functionality to the Document class.

--- muniscrape/civic_plus_site.py
+++ muniscrape/civic_plus_site.py
@@ -1,60 +1,61 @@
 """
 TITLE: CivicPlusSite
 AUTHOR: Amy DiPierro
-VERSION: 2020-06-24
-USAGE: From the command line type 'python3 civic_plus_site.py subdomain begin_date end_date'
+VERSION: 2020-06-26
+USAGE: From the command line, type 'python3 civic_plus_site.py'
+        then enter a required subdomain and option start date and end date,
         where a subdomain is a string corresponding to the first
-        portion of each subdomain and begin_date and end_date are written in the
-        form yyyymmdd. All three items are required.
-
-Example calls:
-    python3 civic_plus_site.py id-kuna 20190101 202006030
-    python3 civic_plus_site.py ga-savannah 20190101 20200101
+        portion of each subdomain and start date and end date are written in the
+        form yyyymmdd. Subdomain is required, but start date and end date have default values.
 
 This script scrapes agendas and minutes from CivicPlus Agenda Center websites.
 It is an object-oriented rewrite of civicplus.py
 
-Output: pdfs of documents
+Input: A CivicPlus subdomain
+Returns: A list of documents found on that subdomain in the given time range
 
 """
 # Libraries
 
 import logging
+import re
+from datetime import datetime
+
 import bs4
 import requests
 from retrying import retry
-import re
 
 #TODO: Get logger to work
 logger = logging.getLogger(__name__)
 
 class CivicPlusSite:
     """
-    In its current configuration, the CivicPlusSite object downloads all documents
+    In its current configuration, the CivicPlusSite object returns a list of all documents
     from a given website in a given date range.
     """
+    base_url = "civicplus.com"
 
-    base_url = "civicplus.com/AgendaCenter"
-
-    def __init__(self, subdomain, begin_date, end_date):
-        self.url = "https://{}.{}".format(subdomain, self.base_url)
-        self.begin_date = begin_date
-        self.end_date = end_date
+    def __init__(self, subdomain):
+        self.url = "https://{}.{}/AgendaCenter".format(subdomain, self.base_url)
+        self.runtime = str(datetime.date(datetime.utcnow())).replace('-', '')
 
     # Public interface (used by calling code)
-    def scrape(self):
+    def scrape(self, start_date, end_date):
+        if start_date == '':
+            start_date = self.runtime
+        if end_date == '':
+            end_date = self.runtime
         logger.info("START SCRAPE - {}".format(self.url))
         html = self._get_html()
         soup = self._make_soup(html)
-        post_params = self._get_post_params(soup)
+        post_params = self._get_post_params(soup, start_date, end_date)
         document_stubs = self._get_all_docs(post_params)
-        filtered_stubs = self._filter_docs(document_stubs, begin_date, end_date)
-        document_links = self._make_document_links(filtered_stubs)
-        metadata = self._get_metadata_dict(document_links)
-        self.download_documents(metadata)
+        filtered_stubs = self._filter_docs(document_stubs, start_date, end_date)
         logger.info("END SCRAPE - {}".format(self.url))
+        print(self._make_document_links(filtered_stubs))
+        return self._make_document_links(filtered_stubs)
 
-    # Private methods (used by the class itself)
+    # Private methods
 
     @retry(
         stop_max_attempt_number=3,
@@ -73,7 +74,7 @@ class CivicPlusSite:
             response = requests.get(self.url)
             return response.text
         except:
-            print("The url could not be reached.")
+            print("The url", self.url, "could not be reached.")
             return ""
 
     def _make_soup(self, html):
@@ -85,10 +86,10 @@ class CivicPlusSite:
         """
         return bs4.BeautifulSoup(html, 'html.parser')
 
-    def _get_post_params(self, soup):
+    def _get_post_params(self, soup, start_date, end_date):
         """
         Given parsed html text grabs the bits of html -- a year and a cat_id -- needed
-        for the POST request.
+        for the POST request. Filters the list by the year in start_date and end_date.
 
         Input: Parsed text
         Returns: A dictionary of years and cat_ids
@@ -100,9 +101,12 @@ class CivicPlusSite:
         for element in year_elements:
             link = element.attrs['href']
             year = re.search(r"(?<=\()\d{4}(?=,)", link).group(0)
-            cat_id = re.findall(r"\d+(?=,.*')", link)[1]
-            cat_ids.append(cat_id)
-            years_cat_id[year] = cat_ids
+            start_year = re.search(r"^\d{4}", start_date).group(0)
+            end_year = re.search(r"^\d{4}", start_date).group(0)
+            if start_year <= year <= end_year:
+                cat_id = re.findall(r"\d+(?=,.*')", link)[1]
+                cat_ids.append(cat_id)
+                years_cat_id[year] = cat_ids
 
         # Remove duplicates
         years_cat_id_2 = {}
@@ -122,14 +126,13 @@ class CivicPlusSite:
         wait_exponential_multiplier=1000,
         wait_exponential_max=10000
     )
-
     def _get_all_docs(self, post_params):
         """
         Given a dictionary and page URL, makes a post request and harvests all of the links from
         the response to that request.
 
         Input: Dictionary of years and catIDs, page URL for POST request
-        Returns: A dictionary of years and catIDs
+        Returns: A list of lists of document URL stubs
         """
         page = "{}/UpdateCategoryList".format(self.url)
         # Get links
@@ -146,7 +149,17 @@ class CivicPlusSite:
                 except:
                     print("In get_all_docs: The POST request failed.")
 
-        return end_links
+        # Remove lists for which no links have been found
+        for list in all_links:
+            if list == []:
+                all_links.remove(list)
+            for stub in list:
+                if stub == []:
+                    list.remove(stub)
+                elif stub == "":
+                    list.remove(stub)
+
+        return all_links
 
     def _get_links(self, soup):
         """
@@ -163,86 +176,55 @@ class CivicPlusSite:
                 end_links.append(link.get('href'))
                 end_links = list(filter(None, end_links))
                 for end_link in end_links:
-                    if ('true' in end_link) or ('http' in end_link) or ('Previous' in end_link) or (
-                            'DocumentCenter' in end_link):
+                    if ('true' in end_link) or ('http' in end_link) or ('Previous' in end_link) or ('DocumentCenter' in end_link):
                         end_links.remove(end_link)
 
         return end_links
 
-    def _filter_docs(self, document_stubs, begin_date, end_date):
-        # Filter the docs by date
+    def _filter_docs(self, document_stubs, start_date, end_date):
+        """
+        Filters documents to the provided date range.
+
+        :param document_stubs: A list of document stubs
+        :param start_date: The earliest date of documents to return
+        :param end_date: The latest date of documents to return
+        :return: A list of document stubs filtered by date range
+        """
         filtered_stubs = []
-        for stub in document_stubs:
-            month = re.search(r"(?<=_)\d{2}(?=\d{6}-)", stub).group(0)
-            day = re.search(r"(?<=_\d{2})\d{2}(?=\d{4}-)", stub).group(0)
-            year = re.search(r"(?<=\d)\d{4}(?=-)", stub).group(0)
-            date = int("{}{}{}".format(year, month, day))
-            if int(begin_date) <= date <= int(end_date):
-                filtered_stubs.append(stub)
+        for list in document_stubs:
+            for stub in list:
+                if re.search(r"(?<=_)\d{2}(?=\d{6}-)", stub) is None:
+                    month = "no_month"
+                else:
+                    month = re.search(r"(?<=_)\d{2}(?=\d{6}-)", stub).group(0)
+                if re.search(r"(?<=_\d{2})\d{2}(?=\d{4}-)", stub) is None:
+                    day = "no_day"
+                else:
+                    day = re.search(r"(?<=_\d{2})\d{2}(?=\d{4}-)", stub).group(0)
+                if re.search(r"(?<=\d)\d{4}(?=-)", stub) is None:
+                    year = "no_year"
+                else:
+                    year = re.search(r"(?<=\d)\d{4}(?=-)", stub).group(0)
+                date = "{}{}{}".format(year, month, day)
+                if "no" in date:
+                    continue
+                if int(start_date) <= int(date) <= int(end_date):
+                    filtered_stubs.append(stub)
 
-        return filtered_stubs
+        # Remove duplicates
+        filtered_stubs_deduped = []
+        for stub in filtered_stubs:
+            if stub not in filtered_stubs_deduped:
+                filtered_stubs_deduped.append(stub)
 
-
-
-    def _get_metadata_dict(self, document_links):
-        """
-        Creates a dictionary of metadata from the link of each document URL, then
-        writes this metadata dictionary to a csv.
-
-        Input: A list of document URLs
-        Returns: A dictionary of metadata
-        Output: A csv
-        """
-
-        # Make the metadata dictionary
-
-        meta = {}
-        place_list = []
-        state_or_province_list = []
-        date_list = []
-        doc_type_list = []
-        meeting_id_list = []
-        url_list = []
-        scraper_list = []
-        doc_format_list = []
-
-        if len(document_links) > 0:
-
-            for document in document_links:
-                meta = self._get_metadata('place', document, r"(?<=-)\w+(?=\.)", place_list, meta)
-                meta = self._get_metadata('state_or_province', document, r"(?<=//)\w{2}(?=-)", state_or_province_list,
-                                    meta)
-                meta = self._get_metadata('date', document, r"(?<=_)\w{8}(?=-)", date_list, meta)
-                meta = self._get_metadata('doc_type', document, r"(?<=e/)\w+(?=/_)", doc_type_list, meta)
-                meta = self._get_metadata('meeting_id', document, r"(?<=/_).+$", meeting_id_list, meta)
-
-                url_list.append(document)
-                meta['url'] = url_list
-
-                scraper_list.append('civicplus')
-                meta['scraper'] = scraper_list
-
-                doc_format_list.append('pdf')
-                meta['doc_format'] = doc_format_list
-
-        else:
-            meta['place'] = ['no_doc_links']
-            meta['state_or_province'] = ['no_doc_links']
-            meta['date'] = ['no_doc_links']
-            meta['doc_type'] = ['no_doc_links']
-            meta['meeting_id'] = ['no_doc_links']
-            meta['url'] = ['no_doc_links']
-            meta['scraper'] = ['civicplus']
-            meta['doc_format'] = ['pdf']
-
-        return meta
+        return filtered_stubs_deduped
 
     def _make_document_links(self, document_stubs):
         """
         Combines a base URL and a list of partial document links (document_stubs)
         to make full urls.
 
-        Input: A base URL and a list of partial document links (document_stubs)
+        Input: A list of partial document links (document_stubs)
         Returns: A list of full URLs
         """
         url_list = []
@@ -254,54 +236,9 @@ class CivicPlusSite:
 
         return url_list
 
-
-    # TODO: Consider other uses for this metadata
-    def _get_metadata(self, key, document, regex, list, meta):
-        """
-        Performs error handling in the case that certain metadata elements are not extractable from a given URL.
-
-        Input: Elements needed to extract metadata
-        Returns: A dictionary
-        """
-        try:
-            item = re.search(regex, document).group(0)
-            list.append(item)
-            meta[key] = list
-            return meta
-        except AttributeError as error:
-            print("AttributeError in get_metadata.")
-            missing_field = "no_{}".format(key)
-            list.append(missing_field)
-            meta[key] = list
-            return meta
-
-    def download_documents(self, metadata):
-        """
-        Downloads documents discovered by the previous functions
-
-        Input: The metadata dictionary
-        Output: .pdfs of each document
-        """
-
-        places = metadata['place']
-        state_or_provinces = metadata['state_or_province']
-        meeting_ids = metadata['meeting_id']
-        documents = metadata['url']
-        doc_types = metadata['doc_type']
-
-        for index in range(len(documents)):
-            file_name = "{}_{}_{}_{}.pdf".format(places[index], state_or_provinces[index], doc_types[index], meeting_ids[index])
-            document = documents[index]
-            if document != 'no_doc_links':
-                print("Downloading document: ", document)
-                response = requests.get(document, allow_redirects=True)
-                open(file_name, 'wb').write(response.content)
-
 if __name__ == '__main__':
-    import sys
-    subdomain = sys.argv[1]
-    begin_date = sys.argv[2]
-    end_date = sys.argv[3]
-    site = CivicPlusSite(subdomain, begin_date, end_date)
-    site.scrape()
-
+    subdomain = input("Enter subdomain: ")
+    start_date = input("Enter start date (or nothing): ")
+    end_date = input("Enter end date (or nothing): ")
+    site = CivicPlusSite(subdomain)
+    site.scrape(start_date=start_date, end_date=end_date)
