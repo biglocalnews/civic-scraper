f908fd0ad2cbdeee3713fb0c84f62a1cebdb7cb1
Chris Stock <christopher.h.stock@gmail.com>
2020-07-07 15:33:23 -0700

    added an example use of the interface:

--- civic_scraper/document.py
+++ civic_scraper/document.py
@@ -9,6 +9,7 @@ class Document(object):
             doc_name: str = None,
             committee_name: str = None,
             place_name: str = None,
+            state_abbr: str = None,
             doc_type: str = None,
             meeting_date: datetime.date = None,
             meeting_time: datetime.time = None,
@@ -24,6 +25,7 @@ class Document(object):
         self.doc_name = doc_name
         self.committee_name = committee_name
         self.place_name = place_name
+        self.state_abbr = state_abbr
         self.doc_type = doc_type
         self.meeting_date = meeting_date
         self.meeting_time = meeting_time
@@ -34,6 +36,15 @@ class Document(object):
     def download(self, target_path):
         """
         Download the document to target_path.
+        """
+        # write me!
+        raise NotImplementedError
+
+    def append_metadata(self, target_path, write_header=False):
+        """
+        Append the document metadata in CSV format to target_path.
+        If write_header is True, first write a line containing the header
+        names. If false, only write one line containing the values.
         """
         # write me!
         raise NotImplementedError
@@ -51,10 +62,38 @@ class DocumentList(object):
         # write me!
         raise NotImplementedError
 
-    def download_metadata(self, target_path):
+    def write_metadata(self, target_path):
         """
-        Download metadata about the document list to a csv at target_path.
+        Write metadata about the document list to a csv at target_path.
         """
         # write me!
+        # this can be a wrapper around Document.append_metadata
         raise NotImplementedError
 
+
+if __name__ == '__main__':
+    import datetime
+
+    # instantiate document
+    doc_args = {
+        'url': 'https://sanmateocounty.legistar.com/View.ashx?M=A&ID='
+            '691905&GUID=86500CA4-50DB-493E-ABCA-1C2446B4B2A6',
+        'doc_name': 'Regular Meeting Agenda, Tuesday, Dec. 10, 2019',
+        'committee_name': 'San Mateo County Board of Supervisors',
+        'place_name': 'San Mateo County',
+        'state': 'CA',
+        'doc_type': 'agenda',
+        'meeting_date': datetime.date(year=2019, month=12, day=10),
+        'meeting_time': datetime.time(hour=9),
+        'doc_format': 'pdf',
+        'meeting_id': 'https://sanmateocounty.legistar.com/View.ashx?M=A&ID='
+            '691905&GUID=86500CA4-50DB-493E-ABCA-1C2446B4B2A6',
+        'scraped_by': 'legistar-scraper.py_v2020-07-07',
+    }
+    doc = Document(**doc_args)
+
+    # write document metadata to a csv
+    doc.append_metadata('path/to/metadata.csv', write_header=True)
+
+    # download document
+    doc.download('path/to/doc_location.pdf')
