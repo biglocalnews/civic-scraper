1205a3632b249709aded0fce3de9cd35bef2b1b5
DiPierro <adipier1@gmail.com>
2020-07-16 06:48:14 -0700

    changing Document -> Asset

--- civic_scraper/scrapers/site.py
+++ civic_scraper/scrapers/site.py
@@ -1,13 +1,12 @@
 """
 Base Site class.
 """
-from civic_scraper.document import DocumentList
-
+from civic_scraper.asset import AssetList
 
 class Site(object):
 
-    def scrape(self, **scrape_args) -> DocumentList:
+    def scrape(self, **scrape_args) -> AssetList:
         """
-        Scrape the site and return a DocumentList instance.
+        Scrape the site and return a AssetList instance.
         """
         raise NotImplementedError
