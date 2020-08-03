30faeb6dd65c84ccb608a52084ebd3188f05e467
DiPierro <adipier1@gmail.com>
2020-08-03 08:39:26 -0700

    add signatures of public CivicPlusSite methods

--- civic_scraper/scrapers/site.py
+++ civic_scraper/scrapers/site.py
@@ -3,10 +3,19 @@ Base Site class.
 """
 from civic_scraper.asset import AssetCollection
 
-
 class Site(object):
 
-    def scrape(self, **scrape_args) -> AssetCollection:
+    def scrape(
+            self,
+            start_date=None,
+            end_date=None,
+            download=False,
+            target_dir=None,
+            file_size=None,
+            asset_list=SUPPORTED_ASSET_TYPES,
+            csv_export=None,
+            append=False
+    ): -> AssetCollection:
         """
         Scrape the site and return a AssetList instance.
         """
