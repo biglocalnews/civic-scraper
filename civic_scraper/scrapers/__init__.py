7602646a0a31503bb8b9ebe79e5e2c5db1f312e2
Chris Stock <christopher.h.stock@gmail.com>
2020-08-03 09:35:42 -0400

    updated scrapers/__init__.py

--- civic_scraper/scrapers/__init__.py
+++ civic_scraper/scrapers/__init__.py
@@ -1,3 +1,8 @@
 from .site import Site
 from .civicplus import CivicPlusSite
 from .granicus import GranicusSite
+
+SUPPORTED_SITES = {
+    'granicus': GranicusSite,
+    'civicplus': CivicPlusSite,
+}
