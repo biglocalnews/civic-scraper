2c10278a65549b290edc4847fa660dd2de1dfd30
Serdar Tumgoren <zstumgoren@gmail.com>
2020-08-31 17:38:20 -0700

    Add test for default values and integrate pytest-vcr

--- tests/test_civicplus.py
+++ tests/test_civicplus.py
@@ -1,20 +1,101 @@
 '''
 USAGE: pytest -q test_civicplus.py
 '''
+import datetime
 
 import pytest
 import os.path
 from os import path
 from civic_scraper.scrapers import CivicPlusSite
 
-@pytest.fixture
-def cp():
-    '''Returns a CivicPlusSite instance with base url http://nc-nashcounty.civicplus.com/AgendaCenter'''
-    return CivicPlusSite("http://nc-nashcounty.civicplus.com/AgendaCenter")
 
-def test_init(cp):
-    assert cp.url == "http://nc-nashcounty.civicplus.com/AgendaCenter"
+# TODO: Parametrize is handy but the below is quite complex.
+# We should simplify at the outset by simply testing that one
+# of the returned Asset classes provides the expected values.
+# This has added benefits: It serves as a form of documentation
+# for the available attributes on Asset, and it makes it much
+# easier to tell when a given attribute does not return an expected
+# value (as opposed to trying to compare a large string to another
+# string).
+# As you encounter edge cases, I would create additional tests for
+# for those. And if you find that edge-case tests greatly resemble the
+# code used for the default case, *then* consider using parametrize
+# (but with an eye toward keeping its inputs relatively simple).
+# Similarly, start by hard-coding expected values inside of a test,
+# and then "promote" those values to more reusable fixtures and/or
+# parametrize inputs if they present opportunities for reuse.
 
+
+# Below are some misc TODOs and additional suggestions, in no particular
+# order.
+
+# TODO: Replace all hard-coded user-specific paths with tmp_path and derivations
+# thereof.
+
+# TODO: User pytest-vcr to record the first live test and code expectations
+# based on those static assets
+
+# TODO: Write pseudo code first to define individual test cases
+# e.g. -
+# 1) test minimal, default case first (for this case, let's limit to start/end date,
+# even though these params are not required)
+# 2) test metadata download
+# 3) test file download
+# test targeted downloads (i.e. specifying one or more file types)
+# (I.e. no downloads)
+# test csv_export
+# test csv append behavior
+# etc.
+# etc.
+
+
+# In a separate test, test downloads, although that will
+# likely require mocking
+
+@pytest.mark.vcr()
+def test_scrape_defaults():
+    '''
+    Test default behavior of CivicPlus.scrape
+    '''
+    site_url = "http://nc-nashcounty.civicplus.com/AgendaCenter"
+    start_date = "2020-05-03"
+    end_date = "2020-05-06"
+    expected_meeting_dates = [
+        datetime.date(2020, 5, day)
+        for day in range(3, 7)
+    ]
+    cp = CivicPlusSite(site_url)
+    assets = cp.scrape(start_date, end_date).assets
+    # Check asset count
+    assert len(assets) == 4
+    # Spot check asset attributes.
+    # Start simple, and add more attribute
+    # checks as needed to cover expected
+    # edge cases (or better yet, put those
+    # checks in a separat test).
+    first = assets[0]
+    assert first.url == 'http://nc-nashcounty.civicplus.com/AgendaCenter/ViewFile/Agenda/_05052020-382'
+    assert first.asset_name == None
+    assert first.committee_name == None
+    assert first.place == 'nashcounty'
+    assert first.state_or_province == 'nc'
+    assert first.asset_type == 'agenda'
+    assert first.meeting_date == datetime.date(2020, 5, 5)
+    assert first.meeting_time == None
+    assert first.meeting_id == 'civicplus_nc_nashcounty_no_data'
+    assert first.scraped_by == 'civicplus.py_1.0.0'
+    assert first.content_type == 'application/pdf'
+    assert first.content_length == '19536'
+    # Check that assets are in the correct date range
+    for asset in assets:
+        assert asset.meeting_date in expected_meeting_dates
+    # Check range of asset types
+    expected_asset_types = ['agenda', 'minutes', 'agenda', 'minutes']
+    actual_asset_types = [asset.asset_type for asset in assets]
+    assert expected_asset_types == actual_asset_types
+
+
+"""
 @pytest.mark.parametrize(
     "start_date,end_date,download,target_dir,file_size,asset_list,csv_export,append,length,printed,meeting_range,path_list", [
         (
@@ -103,3 +184,4 @@ def test_scrape(start_date, end_date, download, target_dir, file_size, asset_lis
     # Clean up: Delete the files we've created.
     for file in path_list:
         os.remove(file)
+"""
