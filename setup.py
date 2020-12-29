a8e5788a559449449c415938a4c49b5789113134
Serdar Tumgoren <zstumgoren@gmail.com>
2020-12-28 17:09:20 -0800

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+civic-scraper
+-------------
+
+`civic-scraper` helps download `agendas`_, `minutes`_ and other documents produced by government.
+It includes a command-line tool and reusable Python code to scrape a growing number
+of public agency websites.
+
+* Documentation: http://civic-scraper.readthedocs.io/en/latest/
+* GitHub: https://github.com/biglocalnews/civic-scraper
+* PyPI: https://pypi.python.org/pypi/civic-scraper
+* Free and open source software: `Apache license`_
+
+.. _Apache license: https://github.com/biglocalnews/civic-scraper/blob/master/LICENSE
+.. _agendas: https://en.wikipedia.org/wiki/Agenda_(meeting)
+.. _minutes: https://en.wikipedia.org/wiki/Minutes
+
+Install civic-scraper_::
+
+   pip install civic-scraper
+
+...and start scraping from the command line::
+
+   # Scrape today's agendas and minutes from a CivicPlus site
+   civic-scraper scrape --download --url http://nc-nashcounty.civicplus.com/AgendaCenter
+
+Or in a script::
+
+  # Scrape today's agendas and minutes from a CivicPlus site
+  from civic_scraper.platforms import CivicPlusSite
+  url = "http://nc-nashcounty.civicplus.com/AgendaCenter"
+  site = CivicPlusSite(url)
+  site.scrape(download=True)
+
+"""
+
 import os
 from setuptools import setup, find_packages
-
-
-def read_reqs(name):
-    dir = os.path.dirname(__file__)
-    reqs_path = os.path.join(dir, name)
-    with open(reqs_path) as fh:
-        reqs = [line.strip() for line in fh.readlines()]
-        return reqs
 
 requirements = [
     'bs4',
@@ -15,8 +45,6 @@ requirements = [
     'click-option-group',
     'requests',
 ]
-
-#requirements = read_reqs('requirements.txt')
 
 test_requirements = [
     'flake8',
@@ -29,7 +57,7 @@ setup(
     name='civic-scraper',
     version='0.1.0',
     description="Command-line tool and library for scraping government agendas, minutes and other public records.",
-    #long_description=__doc__,
+    long_description=__doc__,
     author="Serdar Tumgoren",
     author_email='zstumgoren@gmail.com',
     url='https://github.com/biglocalnews/civic-scraper',
