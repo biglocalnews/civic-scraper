84d237cfca747572b86996d9426d708447b7b4a0
Fatima Gomez <fatima.gomez@datamade.us>
2021-12-17 15:10:44 -0600

    parse rss url

--- civic_scraper/platforms/granicus/site.py
+++ civic_scraper/platforms/granicus/site.py
@@ -1,8 +1,15 @@
 from requests import Session
+import feedparser
 
 def fetch_url(session, url, **kwargs):
     response = session.get(url, headers=headers)
     return response.text
+
+def parse_rss(url):
+    parsed_rss = feedparser.parse(url)
+    summary = parsed_rss['feed']['summary']
+    print(summary)
+    return summary
 
 if __name__ == '__main__':
     session = Session()
@@ -10,7 +17,8 @@ if __name__ == '__main__':
 
     url = 'https://brookhavencityga.iqm2.com/Citizens/default.aspx/Services/RSS.aspx?Feed=Calendar'
 
-    fetch_url(session, url, **headers)
+    rss_text = fetch_url(session, url, **headers)
+    parse_rss(rss_text)
 
 
 
