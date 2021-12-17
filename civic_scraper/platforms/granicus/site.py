9424490935f50534999bd2778667f0cb5f81f0d0
Fatima Gomez <fatima.gomez@datamade.us>
2021-12-17 14:15:39 -0600

    refactoring

--- civic_scraper/platforms/granicus/site.py
+++ civic_scraper/platforms/granicus/site.py
@@ -1,14 +1,16 @@
 from requests import Session
 
-def fetch_url(url = 'https://brookhavencityga.iqm2.com/Citizens/default.aspx', **kwargs):
-    session = Session()
-    headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+def fetch_url(session, url, **kwargs):
     response = session.get(url, headers=headers)
-
-    print(response.text)
-
-    return response
+    return response.text
 
 if __name__ == '__main__':
-    fetch_url()
+    session = Session()
+    headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+
+    url = 'https://brookhavencityga.iqm2.com/Citizens/default.aspx/Services/RSS.aspx?Feed=Calendar'
+
+    fetch_url(session, url, **headers)
+
+
 
