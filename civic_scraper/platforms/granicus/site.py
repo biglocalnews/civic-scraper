9328ec092edbbb2bf47b35344a03d54d03adb205
Fatima Gomez <fatima.gomez@datamade.us>
2021-12-17 12:48:43 -0600

    remove breakpoint

--- civic_scraper/platforms/granicus/site.py
+++ civic_scraper/platforms/granicus/site.py
@@ -5,8 +5,6 @@ def fetch_url(url = 'https://brookhavencityga.iqm2.com/Citizens/default.aspx', *
     headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
     response = session.get(url, headers=headers)
 
-    breakpoint()
-
     print(response.text)
 
     return response
