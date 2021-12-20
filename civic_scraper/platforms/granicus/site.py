f47103a19b5094aa301ae095639cd7f4cc2e1557
Fatima Gomez <fatima.gomez@datamade.us>
2021-12-20 11:57:32 -0600

    ping the correct url

--- civic_scraper/platforms/granicus/site.py
+++ civic_scraper/platforms/granicus/site.py
@@ -1,5 +1,6 @@
 from requests import Session
 import feedparser
+import pprint
 
 def fetch_url(session, url, **kwargs):
     response = session.get(url, headers=headers)
@@ -7,18 +8,15 @@ def fetch_url(session, url, **kwargs):
 
 def parse_rss(url):
     parsed_rss = feedparser.parse(url)
-    summary = parsed_rss['feed']['summary']
-    print(summary)
-    return summary
+    pprint.pprint(parsed_rss)
+    return parsed_rss
 
 if __name__ == '__main__':
     session = Session()
     headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
-    url = 'https://brookhavencityga.iqm2.com/Citizens/default.aspx/Services/RSS.aspx?Feed=Calendar'
+    url = 'https://brookhavencityga.iqm2.com/Services/RSS.aspx?Feed=Calendar'
 
     rss_text = fetch_url(session, url, **headers)
     parse_rss(url)
-
-
 
