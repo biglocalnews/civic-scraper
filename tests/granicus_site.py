29730de79f0d4f2389fb69b241ccb6b85ccdc2bb
Fatima Gomez <fatima.gomez@datamade.us>
2022-01-04 09:53:48 -0600

    update config dict for granicus site testing

--- tests/granicus_site.py
+++ tests/granicus_site.py
@@ -6,19 +6,28 @@ logging.basicConfig(level="DEBUG")
 
 granicus_sites = [
     {'site': 'https://brookhavencityga.iqm2.com/Services/RSS.aspx?Feed=Calendar',
-     'config': {},
+     'config': {
+        'place': 'brookhaven',
+        'state_or_province': 'ga',
+     },
     },
     {'site': 'https://eastpointcityga.iqm2.com/Services/RSS.aspx?Feed=Calendar',
-     'config': {},
+     'config': {
+        'place': 'east_point',
+        'state_or_province': 'ga',
+     },
     },
     {'site': 'https://atlantacityga.iqm2.com/Services/RSS.aspx?Feed=Calendar',
-     'config': {},
+     'config': {
+        'place': 'atlanta',
+        'state_or_province': 'ga',
+     },
     },
 ]
 
 def granicus_integration():
     for obj in granicus_sites:
-        scraper = GranicusSite(obj['site'])
+        scraper = GranicusSite(obj['site'], **obj['config'])
         data = scraper.scrape()
         assert len(data) > 0
 
