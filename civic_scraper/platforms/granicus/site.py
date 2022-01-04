9bcd1b2e1c2a41782b2cd0a6ec36c1f6fba13058
Fatima Gomez <fatima.gomez@datamade.us>
2022-01-04 09:49:28 -0600

    add place and state_or_province to config

--- civic_scraper/platforms/granicus/site.py
+++ civic_scraper/platforms/granicus/site.py
@@ -11,9 +11,11 @@ from urllib.parse import urlparse, parse_qs
 
 
 class GranicusSite(base.Site):
-    def __init__(self, rss_url, cache=Cache()):
+    def __init__(self, rss_url, place=None, state_or_province=None, cache=Cache()):
         self.url = rss_url
         self.granicus_instance = urlparse(rss_url).netloc.split('.')[0]
+        self.place = place
+        self.state_or_province = state_or_province
         self.cache = cache
 
     def create_asset(self, entry):
@@ -24,18 +26,17 @@ class GranicusSite(base.Site):
         meeting_url = entry['link']
         query_dict = parse_qs(urlparse(meeting_url).query)
 
+        # entries for a single granicus instance might use different query params
         if 'ID' in query_dict.keys():
             meeting_id = 'granicus_{}_{}'.format(self.granicus_instance, query_dict['ID'][0])
         else:
             meeting_id = 'granicus_{}_{}'.format(self.granicus_instance, query_dict['MeetingID'][0])
 
-        # breakpoint()
-
         e = {'url': self.url,
              'asset_name': asset_name,
              'committee_name': committee_name,
-             'place': None, # in Legistar, 'place' sometimes None; ok for all granicus 'place' missing?
-             'state_or_province': None,
+             'place': self.place,
+             'state_or_province': self.state_or_province,
              'asset_type': asset_type,
              'meeting_date': meeting_datetime.date(),
              'meeting_time': meeting_datetime.time(),
@@ -43,7 +44,7 @@ class GranicusSite(base.Site):
              'scraped_by': f'civic-scraper_{civic_scraper.__version__}',
              'content_type': 'txt',
              'content_length': None,
-        }
+            }
         return Asset(**e)
 
     def scrape(self, download=True):
@@ -51,7 +52,6 @@ class GranicusSite(base.Site):
         session.headers.update({"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"})
 
         parsed_rss = feedparser.parse(self.url)
-        # breakpoint()
 
         ac = AssetCollection()
         assets = [self.create_asset(e) for e in parsed_rss['entries']]
