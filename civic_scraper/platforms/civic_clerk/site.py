af3739f71a9c887b0c528561604c73391175d705
Fatima Gomez <fatima.gomez@datamade.us>
2022-01-05 12:38:11 -0600

    successful response from event link url

--- civic_scraper/platforms/civic_clerk/site.py
+++ civic_scraper/platforms/civic_clerk/site.py
@@ -1,3 +1,5 @@
+import re
+
 from io import StringIO
 from lxml import etree
 from requests import Session
@@ -9,7 +11,20 @@ def scrape(url):
 
     parser = etree.HTMLParser()
     tree = etree.parse(StringIO(response.text), parser)
-    # breakpoint()
+
+    links = tree.xpath("//table[@id='aspxroundpanelRecent2_ASPxPanel4_grdEventsRecent2_DXMainTable']/tr[@class='dxgvDataRow_CustomThemeModerno']/td[1]//a")
+    for link in links:
+        href = link.attrib['href']
+        pattern = '.*?\((?P<id>.*?),.*'
+        match = re.match(pattern, href)
+
+        link_id = match.group('id')
+        event_url = f'https://chaffeecoco.civicclerk.com/Web/Player.aspx?id={link_id}&key=-1&mod=-1&mk=-1&nov=0'
+
+        response = session.get(event_url)
+
+        ## wait on this
+        # parse out info from this page
 
 if __name__ == '__main__':
     url = 'https://chaffeecoco.civicclerk.com/web/home.aspx'
