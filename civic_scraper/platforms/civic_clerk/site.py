c50e3e2eb2e1f12ca651ff33b668055a516d406f
Fatima Gomez <fatima.gomez@datamade.us>
2022-01-11 10:17:58 -0600

    getting event info from each row in table

--- civic_scraper/platforms/civic_clerk/site.py
+++ civic_scraper/platforms/civic_clerk/site.py
@@ -1,5 +1,6 @@
 import re
 
+from datetime import datetime
 from io import StringIO
 from lxml import etree
 from requests import Session
@@ -12,16 +13,59 @@ def scrape(url):
     parser = etree.HTMLParser()
     tree = etree.parse(StringIO(response.text), parser)
 
-    links = tree.xpath("//table[@id='aspxroundpanelRecent2_ASPxPanel4_grdEventsRecent2_DXMainTable']/tr[@class='dxgvDataRow_CustomThemeModerno']/td[1]//a")
-    for link in links:
+    # only the first <=10 events are here; pagination to be handled later
+    events = tree.xpath("//table[@id='aspxroundpanelRecent2_ASPxPanel4_grdEventsRecent2_DXMainTable']/tr[@class='dxgvDataRow_CustomThemeModerno']")
+
+    for event in events:
+        # grab commitee name, link, & time/date from row
+        committee_name = event.xpath("./td[1]//text()")[1].strip()
+        str_datetime = event.xpath("./td[2]//text()")[0].strip()
+        meeting_datetime = datetime.strptime(str_datetime, '%m/%d/%Y %I:%M %p')
+
+        # for link in row, scrape all assets
+        link = event.xpath("./td[1]//a")[0]
         href = link.attrib['href']
         pattern = '.*?\((?P<id>.*?),.*'
         match = re.match(pattern, href)
 
-        link_id = match.group('id')
-        event_url = f'https://chaffeecoco.civicclerk.com/Web/Player.aspx?id={link_id}&key=-1&mod=-1&mk=-1&nov=0'
+        meeting_id = match.group('id')
+        event_url = f'https://chaffeecoco.civicclerk.com/Web/DocumentFrame.aspx?id={meeting_id}&mod=-1&player_tab=-2'
+        event_response = session.get(event_url)
 
-        response = session.get(event_url)
+        event_tree = etree.parse(StringIO(event_response.text), parser)
+        event_frame = event_tree.xpath("//iframe")[0].attrib['src']
+
+        event_frame_url = 'https://chaffeecoco.civicclerk.com' + event_frame
+        frame_response = session.get(event_frame_url)
+        frame_tree = etree.parse(StringIO(frame_response.text), parser)
+
+        agenda_items = frame_tree.xpath("//tr[./td[@class='dx-wrap dxtl dxtl__B0' and not(@colspan)]]")
+        for item in agenda_items:
+            # dig into item > last td (use class) to get agenda text
+            # asset_name =
+            # asset_type =
+            # dig into link_tr to get link for agenda item
+            link_tr = item.xpath("./following-sibling::tr[1]")
+
+            # def create_asset(self, entry):
+                e = {'url': event_url,
+            #          'asset_name': asset_name,
+                     'committee_name': committee_name,
+            #          'place': self.place, # config
+            #          'state_or_province': self.state_or_province, # config
+            #          'asset_type': asset_type,
+                     'meeting_date': meeting_datetime.date(),
+                     'meeting_time': meeting_datetime.time(),
+                     'meeting_id': meeting_id,
+                     'scraped_by': f'civic-scraper_{civic_scraper.__version__}',
+                     'content_type': 'txt',
+                     'content_length': None,
+                    }
+            #     return Asset(**e)
+
+        agenda_name = frame_tree.xpath("//span[@id='lblAgendaName']")
+
+        breakpoint()
 
         ## wait on this
         # parse out info from this page
