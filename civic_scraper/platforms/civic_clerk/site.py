2ca4a4aeac476eb06f90c044b4ce1e974fce50df
Forest Gregg <fgregg@datamade.us>
2022-02-04 13:43:32 -0500

    session is attribute, etree.parse -> lxml.html.fromstring

--- civic_scraper/platforms/civic_clerk/site.py
+++ civic_scraper/platforms/civic_clerk/site.py
@@ -1,8 +1,7 @@
 import re
 
 from datetime import datetime
-from io import StringIO
-from lxml import etree
+import lxml.html
 from requests import Session
 from pathlib import Path
 from urllib.parse import urlparse
@@ -15,12 +14,16 @@ from civic_scraper.base.cache import Cache
 
 class CivicClerkSite(base.Site):
     def __init__(self, url, place=None, state_or_province=None, cache=Cache()):
+
         self.url = url
         self.base_url = 'https://' + urlparse(url).netloc
         self.civicclerk_instance = urlparse(url).netloc.split('.')[0]
         self.place = place
         self.state_or_province = state_or_province
         self.cache = cache
+
+        self.session = Session()
+        self.session.headers.update({"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"})
 
     def create_asset(self, asset, committee_name, meeting_datetime, meeting_id):
         asset_url, asset_name = asset
@@ -48,14 +51,14 @@ class CivicClerkSite(base.Site):
         match = re.match(pattern, href)
         return (match.group('id'), 'civicclerk_{}_{}'.format(self.civicclerk_instance, match.group('id')))
 
-    def get_agenda_items(self, text, parser, session):
-        event_tree = etree.parse(StringIO(text), parser)
+    def get_agenda_items(self, text):
+        event_tree = lxml.html.fromstrinfg(text)
 
         event_frame = event_tree.xpath("//iframe[@id='docViewer']")[0]
         event_frame_url = self.base_url + event_frame.attrib['src']
 
-        frame_response = session.get(event_frame_url)
-        frame_tree = etree.parse(StringIO(frame_response.text), parser)
+        frame_response = self.session.get(event_frame_url)
+        frame_tree = lxml.html.fromstring(frame_response.text)
         frame_has_table = True if frame_tree.xpath("//table") else False
 
         if frame_has_table:
@@ -71,12 +74,9 @@ class CivicClerkSite(base.Site):
             return [(event_frame_url, None)]
 
     def scrape(self, download=True):
-        session = Session()
-        session.headers.update({"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"})
-        response = session.get(self.url)
+        response = self.session.get(self.url)
 
-        parser = etree.HTMLParser()
-        tree = etree.parse(StringIO(response.text), parser)
+        tree = lxml.html.fromstring(response.text)
 
         # only the first <=10 events are here; pagination to be handled later
         events = tree.xpath("//table[@id='aspxroundpanelRecent2_ASPxPanel4_grdEventsRecent2_DXMainTable']/tr[@class='dxgvDataRow_CustomThemeModerno']")
@@ -90,9 +90,9 @@ class CivicClerkSite(base.Site):
             meeting_id_num, meeting_id = self.get_meeting_id(event)
 
             event_url = f'{self.base_url}/Web/DocumentFrame.aspx?id={meeting_id_num}&mod=-1&player_tab=-2'
-            event_response = session.get(event_url)
+            event_response = self.session.get(event_url)
 
-            agenda_items = self.get_agenda_items(event_response.text, parser, session)
+            agenda_items = self.get_agenda_items(event_response.text)
 
             if agenda_items:
                 assets = [self.create_asset(a, committee_name, meeting_datetime, meeting_id) for a in agenda_items]
@@ -105,6 +105,6 @@ class CivicClerkSite(base.Site):
             for asset in ac:
                 if asset.url:
                     dir_str = str(asset_dir)
-                    asset.download(target_dir=dir_str, session=session)
+                    asset.download(target_dir=dir_str, session=self.session)
             # parse out info from this page
         return ac
