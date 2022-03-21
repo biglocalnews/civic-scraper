d6555093cf8dc8104e6f9d5d1768e336c6339c5a
palewire <b@palewi.re>
2022-03-21 11:03:50 -0700

    Trim

--- setup.py
+++ setup.py
@@ -127,6 +127,6 @@ setup(
     ],
     test_suite="tests",
     tests_require=test_requirements,
-    setup_requires=["pytest-runner", "setuptools_scm"],
+    setup_requires=["setuptools_scm"],
     use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
 )
