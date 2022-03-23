7fad6142eeb9b0b55b167bbcc532f60426e20fd5
Ben Welsh <b@palewi.re>
2022-03-23 12:49:57 -0700

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,3 @@
-#!/usr/bin/env python
-"""
-civic-scraper
--------------
-
-`civic-scraper` helps download `agendas`_, `minutes`_ and other documents produced by government.
-It includes a command-line tool and reusable Python code to scrape a growing number
-of public agency websites.
-
-* Docs: http://civic-scraper.readthedocs.io/en/latest/
-* GitHub: https://github.com/biglocalnews/civic-scraper
-* PyPI: https://pypi.python.org/pypi/civic-scraper
-* Free and open source software: `Apache license`_
-
-.. _Apache license: https://github.com/biglocalnews/civic-scraper/blob/master/LICENSE
-.. _agendas: https://en.wikipedia.org/wiki/Agenda_(meeting)
-.. _minutes: https://en.wikipedia.org/wiki/Minutes
-
-Basic install and usage
------------------------
-
-
-Install civic-scraper_::
-
-   pip install civic-scraper
-
-...and start scraping from the command line::
-
-   # Scrape today's agendas and minutes from a CivicPlus site
-   civic-scraper scrape --download --url http://nc-nashcounty.civicplus.com/AgendaCenter
-
-Or in a script::
-
-  # Scrape today's agendas and minutes from a CivicPlus site
-  from civic_scraper.platforms import CivicPlusSite
-  url = "http://nc-nashcounty.civicplus.com/AgendaCenter"
-  site = CivicPlusSite(url)
-  site.scrape(download=True)
-
-See the `official docs`_ for more details on usage.
-
-.. _official docs: http://civic-scraper.readthedocs.io/en/latest/
-"""
 import os
 
 from setuptools import find_packages, setup
@@ -80,24 +37,6 @@ def local_version(version):
     """
     return ""
 
-
-requirements = [
-    "bs4",
-    "click",
-    "click-option-group",
-    "feedparser",
-    "requests",
-    "demjson3",
-    "scraper-legistar",
-]
-
-test_requirements = [
-    "flake8",
-    "pytest",
-    "pytest-vcr",
-    "vcrpy",
-]
-
 setup(
     name="civic-scraper",
     description="Tools for downloading agendas, minutes and other documents produced by local government",
@@ -112,7 +51,15 @@ setup(
         [console_scripts]
         civic-scraper=civic_scraper.cli:cli
     """,
-    install_requires=requirements,
+    install_requires=[
+        "bs4",
+        "click",
+        "click-option-group",
+        "feedparser",
+        "requests",
+        "demjson3",
+        "scraper-legistar",
+    ],
     license="Apache 2.0 license",
     zip_safe=False,
     classifiers=[
@@ -122,12 +69,24 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     test_suite="tests",
-    tests_require=test_requirements,
+    tests_require=[
+        "flake8",
+        "pytest",
+        "pytest-vcr",
+        "vcrpy",
+    ],
     setup_requires=["setuptools_scm"],
     use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
+    project_urls={
+        "Documentation": "https://civic-scraper.readthedocs.io",
+        "Maintainer": "https://github.com/biglocalnews",
+        "Source": "https://github.com/biglocalnews/civic-scraper",
+        "Tracker": "https://github.com/biglocalnews/civic-scraper/issues",
+    },
 )
