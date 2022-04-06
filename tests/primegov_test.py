c14863c68be129f921574732bcb406fc83a84023
msj <monkruman.st.jules@datamde.us>
2022-04-06 12:18:23 -0400

    Added scraping for more sites

--- tests/primegov_test.py
+++ tests/primegov_test.py
@@ -1,14 +1,39 @@
 import pytest
 import logging
+
 from civic_scraper.platforms import PrimeGovSite
 
 logging.basicConfig(level="DEBUG")
 
 primegov_sites = [
-    {'site': 'https://lacity.primegov.com/api/meeting/',
+    {'site': 'https://lacity.primegov.com/',
      'config': {
         'place': 'los angeles',
         'state_or_province': 'ca',
+     },
+    },
+    {'site': 'https://santafe.primegov.com/',
+     'config': {
+        'place': 'santa fe',
+        'state_or_province': 'nm',
+     },
+    },
+    {'site': 'https://sanantonio.primegov.com/',
+     'config': {
+        'place': 'san antonio',
+        'state_or_province': 'tx',
+     },
+    },
+    {'site': 'https://ventura.primegov.com/',
+     'config': {
+        'place': 'ventura',
+        'state_or_province': 'ca',
+     },
+    },
+    {'site': 'https://lasvegas.primegov.com/',
+     'config': {
+        'place': 'las vegas',
+        'state_or_province': 'nv',
      },
     },
 ]
@@ -16,7 +41,7 @@ primegov_sites = [
 def primegov_integration():
 
     for obj in primegov_sites:
-        scraper = PrimeGovSite(obj['site'])
+        scraper = PrimeGovSite(obj['site'], **obj['config'])
         data = scraper.scrape()
         assert len(data) > 0
 
