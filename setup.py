4c1a5ef02e3844e7aee68d4213bdaa18fb133c6c
Paige Moody <moodypaige.e@gmail.com>
2025-06-19 15:04:38 -0400

    [Issue 197] Fix readthedocs build (take 2) (#199)

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
