62c53341d0b983b8d7ef41f61ea55a71e9f02788
paigemoody <moodypaige.e@gmail.com>
2025-10-14 15:29:02 -0400

    bump legistar

--- setup.py
+++ setup.py
@@ -59,9 +59,7 @@ setup(
         "feedparser",
         "requests",
         "demjson3",
-        # "scraper-legistar",
-        # TEMP until we get a new release
-        "legistar @ git+https://github.com/opencivicdata/python-legistar-scraper.git@4ca46ef10f15c53005678363ac465d04123e7177#egg=legistar"
+        "scraper-legistar"
     ],
     license="Apache 2.0 license",
     zip_safe=False,
